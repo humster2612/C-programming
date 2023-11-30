b0556bfb15ff2d61e31b33f4b9b5bc0d21bf36d9
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-11-30 01:45:31 +0100

    Add files via upload

---
+++ lab4/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab4
+{
+    class Shape
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine($"Rysowanie shape ({X}, {Y}) o wysokości {Height} i szerokości {Width}");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Rectangle(0, 0, 5, 10));
+
+            shapes.Add(new Triangle(0, 0, 5, 10));
+
+            shapes.Add(new Circle(0, 5, 10));
+
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+                Console.WriteLine();
+            }
+
+        }
+    }
+    class Rectangle : Shape
+    {
+        public Rectangle(int x, int y, int height, int width)
+        {
+            X = x;
+
+            Y = y;
+
+            Height = height;
+
+            Width = width;
+        }
+        public override void Draw()
+        {
+
+            Console.WriteLine($"Rysowanie shape ({X}, {Y}) o wysokości {Height} i szerokości {Width}");
+
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle(int x, int y, int height, int width)
+        {
+            X = x;
+
+            Y = y;
+
+            Height = height;
+
+            Width = width;
+        }
+        
+        public override void Draw()
+        {
+            Console.WriteLine($"Rysowanie shape ({X}, {Y}) o wysokości {Height} i szerokości {Width}");
+        }
+    }
+    class Circle : Shape
+    {
+        public Circle(int x, int y, int radius)
+        {
+            X = x;
+
+            Y = y;
+
+            Height = radius * 2;
+
+            Width = radius * 2;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Rysowanie shape ({X}, {Y}) o radius {Height / 2}");
+        }
+    }
+}
---
+++ zad2lab4(1)/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace zad2lab4
+{
+    abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string PESEL;
+
+        public void SetFirstName(string firstName)
+        {
+
+            FirstName = firstName;
+
+        }
+        public void SetLastName(string lastName)
+        {
+
+            LastName = lastName;
+
+        }
+
+        public void SetPesel(string pesel)
+        {
+
+            PESEL = pesel;
+
+        }
+
+        public abstract int GetAge();
+
+        public abstract string GetGender();
+
+        public abstract string GetEducationInfo();
+
+        public abstract string GetFullName();
+
+        public abstract bool CanGoAloneToHome();
+
+    }
+
+    class Student : Person
+    {
+        public string School;
+
+        private bool canGoAloneToHome;
+
+        public void SetSchool(string school)
+        {
+
+            School = school;
+        }
+        public void SetCanGoHomeAlone(bool canGoHomeAlone)
+        {
+
+            canGoAloneToHome = canGoHomeAlone;
+        }
+        public void ChangeSchool(string newSchool)
+        {
+
+            School = newSchool;
+        }
+
+        public override int GetAge()
+        {
+
+            return 18;
+        }
+
+        public override string GetGender()
+        {
+
+            return PESEL[9] % 2 == 0 ? "Kobieta" : "Mężczyzna";
+        }
+        public override string GetEducationInfo()
+        {
+
+
+            return $"Student w {School}";
+        }
+        public override string GetFullName()
+        {
+
+            return $"{FirstName} {LastName}";
+        }
+        public override bool CanGoAloneToHome()
+        {
+
+            return canGoAloneToHome || GetAge() >= 12;
+        }
+
+        public string Info()
+        {
+            if (GetAge() < 12 && !canGoAloneToHome)
+            {
+
+                return "Osoby poniżej 12 roku życia nie mogą wrócić same, chyba że mają pozwolenie";
+            }
+            else
+            {
+
+                return "Uczeń może wrócić do domu sam";
+            }
+        }
+    }
+
+    class Teacher : Student
+    {
+        private string AcademicTitle;
+
+        private List<Student> SubordinateStudents;
+
+        public Teacher(string academicTitle)
+        {
+            AcademicTitle = academicTitle;
+
+            SubordinateStudents = new List<Student>();
+        }
+        public void AddSubordinateStudent(Student student)
+        {
+
+            SubordinateStudents.Add(student);
+        }
+        public void WhichStudentCanGoHomeAlone()
+        {
+            Console.WriteLine("Uczniowie, którzy mogą wrócić do domu sami:");
+
+            foreach (var student in SubordinateStudents)
+            {
+
+                if (student.CanGoAloneToHome())
+                {
+                    Console.WriteLine($"{student.GetFullName()} - {student.GetGender()} - {student.Info()}");
+                }
+
+            }
+        }
+        public void PrintTeacherAndStudentsInfo(string dayOfWeek)
+        {
+            Console.WriteLine($"{School} Na: {dayOfWeek}");
+            Console.WriteLine($"Nauczyciel: {AcademicTitle} {FirstName} {LastName}");
+            Console.WriteLine("Lista uczniów:");
+
+            int lp = 1;
+
+            foreach (var student in SubordinateStudents)
+            {
+                Console.WriteLine($"{lp}. {student.GetFullName()} - {student.GetGender()} - {student.Info()}");
+                lp++;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("dr.");
+            teacher.SetFirstName("Iwona");
+            teacher.SetLastName("Bibik");
+            teacher.SetSchool("Szkoła");
+
+
+            Student student1 = new Student();
+            student1.SetFirstName("Olha");
+            student1.SetLastName("Movchan");
+            student1.SetSchool("Szkoła");
+            student1.SetCanGoHomeAlone(false);
+
+            Student student2 = new Student();
+            student2.SetFirstName("Polina");
+            student2.SetLastName("Cybulska");
+            student2.SetSchool("Szkoła");
+            student2.SetCanGoHomeAlone(true);
+
+            teacher.AddSubordinateStudent(student1);
+
+            teacher.AddSubordinateStudent(student2);
+
+            teacher.PrintTeacherAndStudentsInfo("Whensday");
+
+            teacher.WhichStudentCanGoHomeAlone();
+        }
+    }
+}
