deb309e943bbda377c90ec107dc3104f8118c896
humster2612 <129902401+humster2612@users.noreply.github.com>
2024-01-16 01:39:30 +0100

    Add files via upload

---
+++ lab6zad2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę pliku do wczytania: ");
+        string nazwaPliku = Console.ReadLine();
+
+        WczytajZawartoscPliku(nazwaPliku);
+    }
+
+    static void WczytajZawartoscPliku(string nazwaPliku)
+    {
+        try
+        {
+            if (File.Exists(nazwaPliku))
+            {
+                using (StreamReader sr = new StreamReader(nazwaPliku))
+                {
+                    string zawartosc = sr.ReadToEnd();
+                    Console.WriteLine($"Zawartość pliku {nazwaPliku}:\n{zawartosc}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Plik o nazwie {nazwaPliku} nie istnieje.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas wczytywania pliku: {ex.Message}");
+        }
+    }
+}
+
