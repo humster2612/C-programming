413dca27e0ed52a09f854aa5250139bbcd91d42b
humster2612 <129902401+humster2612@users.noreply.github.com>
2024-01-16 01:36:29 +0100

    Add files via upload

---
+++ zad1lab6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj numer albumu: ");
+        string numerAlbumu = Console.ReadLine();
+
+        Console.Write("Podaj nazwę pliku do zapisu: ");
+        string nazwaPliku = Console.ReadLine();
+
+        ZapiszNumerAlbumuDoPliku(numerAlbumu, nazwaPliku);
+
+        Console.WriteLine($"Numer albumu został zapisany do pliku {nazwaPliku}.");
+    }
+
+    static void ZapiszNumerAlbumuDoPliku(string numerAlbumu, string nazwaPliku)
+    {
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(nazwaPliku))
+            {
+                sw.WriteLine(numerAlbumu);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas zapisu do pliku: {ex.Message}");
+        }
+    }
+}
+
